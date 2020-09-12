0cf9bc97b52be41db90b8a83e83e6b810fe731d8
Takayuki Maeda <takoyaki0316@gmail.com>
2020-09-12 17:04:23 +0900

    change DB host to A

--- isuumo/webapp/go/main.go
+++ isuumo/webapp/go/main.go
@@ -200,7 +200,7 @@ func (r *RecordMapper) Err() error {
 
 func NewMySQLConnectionEnv() *MySQLConnectionEnv {
 	return &MySQLConnectionEnv{
-		Host:     getEnv("MYSQL_HOST", "127.0.0.1"),
+		Host:     getEnv("MYSQL_HOST", "10.160.10.101"),
 		Port:     getEnv("MYSQL_PORT", "3306"),
 		User:     getEnv("MYSQL_USER", "isucon"),
 		DBName:   getEnv("MYSQL_DBNAME", "isuumo"),
