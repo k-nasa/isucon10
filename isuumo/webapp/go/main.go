266de6deef0feb6bdf4ab8dd92a9ba26337d4c28
k-nasa <htilcs1115@gmail.com>
2020-09-12 16:12:03 +0900

    update

--- isuumo/webapp/go/main.go
+++ isuumo/webapp/go/main.go
@@ -862,9 +862,12 @@ func searchEstateNazotte(c echo.Context) error {
 		return c.NoContent(http.StatusBadRequest)
 	}
 
+	coodinatesText := coordinates.coordinatesToText()
+
 	b := coordinates.getBoundingBox()
 	estatesInBoundingBox := []Estate{}
-	query := `SELECT * FROM estate WHERE latitude <= ? AND latitude >= ? AND longitude <= ? AND longitude >= ? ORDER BY popularity DESC, id ASC`
+	query := fmt.Sprintf(`SELECT * FROM estate as e WHERE latitude <= ? AND latitude >= ? AND longitude <= ? AND longitude >= ? AND  ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(POINT(e.latitude e.longitude))) ORDER BY popularity DESC, id ASC`, coodinatesText)
+
 	err = db.Select(&estatesInBoundingBox, query, b.BottomRightCorner.Latitude, b.TopLeftCorner.Latitude, b.BottomRightCorner.Longitude, b.TopLeftCorner.Longitude)
 	if err == sql.ErrNoRows {
 		c.Echo().Logger.Infof("select * from estate where latitude ...", err)
@@ -874,24 +877,7 @@ func searchEstateNazotte(c echo.Context) error {
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	estatesInPolygon := []Estate{}
-	for _, estate := range estatesInBoundingBox {
-		validatedEstate := Estate{}
-
-		point := fmt.Sprintf("'POINT(%f %f)'", estate.Latitude, estate.Longitude)
-		query := fmt.Sprintf(`SELECT * FROM estate WHERE id = ? AND ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(%s))`, coordinates.coordinatesToText(), point)
-		err = db.Get(&validatedEstate, query, estate.ID)
-		if err != nil {
-			if err == sql.ErrNoRows {
-				continue
-			} else {
-				c.Echo().Logger.Errorf("db access is failed on executing validate if estate is in polygon : %v", err)
-				return c.NoContent(http.StatusInternalServerError)
-			}
-		} else {
-			estatesInPolygon = append(estatesInPolygon, validatedEstate)
-		}
-	}
+	estatesInPolygon := estatesInBoundingBox
 
 	var re EstateSearchResponse
 	re.Estates = []Estate{}
